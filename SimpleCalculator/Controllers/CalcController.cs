c1c5fb7eed39a29052ec4dd29744b6b26fa12a44
Mohitvilasgiri <Mohit@DESKTOP-K7UQJ0F>
2023-04-25 15:56:26 +0530

    Add project files.

---
+++ SimpleCalculator/Controllers/CalcController.cs
@@ -0,0 +1,62 @@
+﻿using SimpleCalculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleCalculator.Controllers
+{
+    public class CalcController : Controller
+    {
+        // GET: Calc
+        public ActionResult Index()
+        {
+            return View(new calc());
+        }
+        [HttpPost]
+
+        public ActionResult Index(calc c, string claculate)
+        {
+            if(claculate == "add") 
+            {
+                c.Totalnumber = c.number1 + c.number2;
+            }
+
+            else if (claculate =="min")
+            {
+                c.Totalnumber = c.number1 - c.number2;
+
+            }
+            else if (claculate == "mul")
+            {
+                c.Totalnumber = c.number1 * c.number2;
+
+            }
+            else if (claculate == "cle")
+            {
+                c.number1 = 0;
+                c.number2=0;
+                c.Totalnumber = 0;
+
+            }
+            else 
+            {
+                try
+                {
+                    c.Totalnumber = c.number1 / c.number2;
+                }
+                catch (Exception e) 
+                {
+                    Response.Write("<script>alert('OOPS !!!')</script>");
+                }
+                
+                
+
+            }
+
+            return View(c);
+        }
+
+    }
+}
---
+++ SimpleCalculator/Models/calc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleCalculator.Models
+{
+    public class calc
+    {
+
+        public int number1 { get; set; }
+        public int number2 { get; set; }
+        public int Totalnumber { get; set; }
+    }
+}
